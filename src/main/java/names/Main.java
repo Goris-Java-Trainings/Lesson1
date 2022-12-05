a2abf5e833c79a9cac92bff2132cfdfcef900cf8
Narek22 <musakhanyan.narek@gmail.com>
2022-12-05 20:37:07 +0400

    br2 commit

--- src/main/java/names/Main.java
+++ src/main/java/names/Main.java
@@ -4,5 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!!");
         System.out.println("new change");
+
+        System.out.println("commit BR2");
     }
 }
